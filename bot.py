4b8c1ae2600718ba65717df9aa9808ba613cc113
Hamis-cell <daniil.merits@gmail.com>
2024-12-21 16:54:34 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,34 @@
+import telebot
+from bot_logic import gen_pass, gen_emodji, flip_coin 
+
+bot = telebot.TeleBot("7786003316:AAEIGcb2wFscbofdmELthxQVnpEMM5S-yuc")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin  ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['pass'])
+def send_password(message):
+    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
+
+@bot.message_handler(commands=['emodji'])
+def send_emodji(message):
+    emodji = gen_emodji()
+    bot.reply_to(message, f"Вот эмоджи': {emodji}")
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, f"Монетка выпала так: {coin}")
+
+print ("бот работает")
+bot.polling()
