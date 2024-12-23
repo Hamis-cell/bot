c97f3fa6dce0197f036d6f44868164649bdab764
Hamis-cell <daniil.merits@gmail.com>
2024-12-23 09:34:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
+import random
 from bot_logic import gen_pass, gen_emodji, flip_coin 
-
+from telebot.types import ReactionTypeEmoji
 bot = telebot.TeleBot("")
     
 @bot.message_handler(commands=['start'])
@@ -17,18 +18,29 @@ def send_bye(message):
 
 @bot.message_handler(commands=['pass'])
 def send_password(message):
-    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+    password = gen_pass(10)  
     bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
 
 @bot.message_handler(commands=['emodji'])
 def send_emodji(message):
     emodji = gen_emodji()
     bot.reply_to(message, f"Вот эмоджи': {emodji}")
+    
 
 @bot.message_handler(commands=['coin'])
 def send_coin(message):
     coin = flip_coin()
     bot.reply_to(message, f"Монетка выпала так: {coin}")
 
+
+@bot.message_handler(func=lambda message: "/coin")
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+@bot.message_reaction_handler(func=lambda message: "/coin")
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
 print ("бот работает")
 bot.polling()
